5e11a8438fcadb230f806e6b31b8f893e2d65b0d
Elliot Voris <elliot@stellar.org>
2025-08-08 12:05:51 -0500

    justify content in a safe way, better admin page visibility

--- src/lib/components/ui/PageContent.svelte
+++ src/lib/components/ui/PageContent.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
     let { children } = $props();
-    import { fade } from 'svelte/transition';
-    import { page } from '$app/state';
 </script>
 
 <div class="flex-auto w-full h-full flex overflow-hidden p-4 text-center">
     <div
         id="page"
-        class="scroll-smooth flex-1 overflow-x-hidden flex flex-col justify-center items-center"
+        class="scroll-smooth flex-1 overflow-x-hidden flex flex-col justify-center-safe items-center"
         style="scrollbar-gutter: auto;"
     >
         <main id="page-content" class="flex">
